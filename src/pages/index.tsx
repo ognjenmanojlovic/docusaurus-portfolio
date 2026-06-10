34fcdcc07bbac8a7b66d885b05be40ae5f36fe62
Ognjen Manojlovic <ogisha@MacBookPro.local>
2026-06-10 04:55:17 +0200

    perf(portfolio): preload key images and improve initial page loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { JSX } from 'react';
 import Layout from '@theme/Layout';
+import Head from '@docusaurus/Head';
 
 import Header from '@site/src/components/header';
 import Hero from '@site/src/components/hero';
@@ -8,10 +9,22 @@ import ProjectHighlights from '@site/src/components/project-highlights';
 import Contact from '@site/src/components/contact';
 import Footer from '@site/src/components/footer';
 
-
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
-    <Layout >
+    <Layout>
+      <Head>
+        <link rel="preload" as="image" href="/img/Manojlovic_Picture.png" />
+        <link rel="preload" as="image" href="/img/projects/screenshots/aws-security-lab/aws-security-lab.png" />
+        <link rel="preload" as="image" href="/img/projects/screenshots/terraform-ansible/terraform-ansible-logo.png" />
+        <link rel="preload" as="image" href="/img/projects/screenshots/fastapi-devsecops/FastAPI-DevSecOps-Platform.png" />
+        <link rel="preload" as="image" href="/img/projects/screenshots/fastapi-kubernetes/FastAPI-Kubernetes-Platform.png" />
+        <link rel="preload" as="image" href="/img/projects/screenshots/soc-lab/SOC-SIEM-Lab.png" />
+        <link rel="preload" as="image" href="/img/projects/conduit.png" />
+        <link rel="preload" as="image" href="/img/projects/babytools.png" />
+        <link rel="preload" as="image" href="/img/projects/juiceshop.png" />
+        <link rel="preload" as="image" href="/img/projects/password-cracking.png" />
+      </Head>
+
       <Header />
       <Hero />
       <MySkills />
